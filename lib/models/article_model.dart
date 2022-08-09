63ef9050f7964ad03c7516ff8769ba993515adbf
Aoutou Islam <js298024@gmail.com>
2022-08-09 22:19:47 +0100

    news_of_the day

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -28,7 +28,8 @@ class Article extends Equatable {
   static List<Article> articles = [
     Article(
         id: '1',
-        title: 'lorem epsom',
+        title:
+            'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ind',
         SubTitle:
             "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release o",
         body:
@@ -43,7 +44,8 @@ class Article extends Equatable {
         created_at: DateTime.now().subtract(const Duration(hours: 5))),
     Article(
       id: '2',
-      title: 'lorem epsom',
+      title:
+          'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ind',
       SubTitle:
           " is a long established fact that a reader will be distracted by the readable content of a page ",
       body:
@@ -75,7 +77,8 @@ class Article extends Equatable {
     ),
     Article(
       id: '50',
-      title: 'lorem epsom',
+      title:
+          'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ind',
       SubTitle:
           " is a long established fact that a reader will be distracted by the readable content of a page ",
       body:
