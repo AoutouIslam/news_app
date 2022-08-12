83f8ba28f2e58294097ed96994e6e62f1d6a65ef
Aoutou Islam <js298024@gmail.com>
2022-08-12 21:32:07 +0100

    created news section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
+import 'package:news_app/screens/article_screen.dart';
 import 'package:news_app/widget/custom_tag.dart';
 import '../widget/bottom_nav_bar.dart';
 import '../widget/image_container.dart';
@@ -28,6 +29,105 @@ class HomeScreen extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           _NewsOfTheDay(article: article),
+          _BreakingNews(articles: Article.articles)
+        ],
+      ),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  final List<Article> articles;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Text(
+                'More News',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  margin: const EdgeInsets.only(right: 10),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        ArticleScreen.routeName,
+                        arguments: articles[index],
+                      );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ImageContainer(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            imageUrl: articles[index].ImageUrl),
+                        const SizedBox(height: 10),
+                        Text(
+                          articles[index].title,
+                          maxLines: 2,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.5),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          'by ${articles[index].author}',
+                          maxLines: 2,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          '${DateTime.now().difference(articles[index].created_at).inHours} hours ago',
+                          maxLines: 2,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(color: Colors.blueAccent),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
